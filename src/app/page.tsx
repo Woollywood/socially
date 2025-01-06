ad7344c78fd3d9f1f15a7264f4309124bc998804
Woollywood <115595112+Woollywood@users.noreply.github.com>
2025-01-06 19:00:12 +0300

    feat: clerk/auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import { NextPage } from 'next';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 const Page: NextPage = () => {
-	return <div></div>;
+	return (
+		<div>
+			<SignedOut>
+				<SignInButton mode='modal' />
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+		</div>
+	);
 };
 
 export default Page;
